147066f436974c7598bd3665c20aaf34df89cdb1
Reece Rose <reecerose18@gmail.com>
2021-08-31 21:10:00 -0400

    trying to get build working

--- web/jest.config.js
+++ web/jest.config.js
@@ -1,5 +1,5 @@
 module.exports = {
-  setupFiles: ['<rootDir>/.jest/setEnvVars.js'],
+  setupFiles: [],
   testPathIgnorePatterns: ['<rootDir>/.next/', '<rootDir>/node_modules/'],
   setupFilesAfterEnv: ['<rootDir>/.jest/setupTests.js'],
   transform: {
