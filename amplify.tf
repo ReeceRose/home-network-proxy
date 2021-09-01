7820f00ca2d938d7f8433f09da5f238889273169
Reece Rose <reecerose18@gmail.com>
2021-08-31 21:53:44 -0400

    UWeb UI work (#8)

--- amplify.tf
+++ amplify.tf
@@ -3,19 +3,6 @@ resource "aws_amplify_app" "web" {
   name       = "Home Network Proxy Web UI"
   repository = "https://github.com/reecerose/home-network-proxy"
 
-  enable_auto_branch_creation = true
-
-  # The default patterns added by the Amplify Console.
-  auto_branch_creation_patterns = [
-    "*",
-    "*/**",
-  ]
-
-  auto_branch_creation_config {
-    # Enable auto build for the created branch.
-    enable_auto_build = true
-  }
-
   # The default build_spec added by the Amplify Console for React.
   build_spec = <<-EOT
     version: 0.1
@@ -51,10 +38,10 @@ resource "aws_amplify_app" "web" {
   }
 }
 
-# resource "aws_amplify_branch" "production" {
-#   app_id      = aws_amplify_app.web.id
-#   branch_name = "main"
+resource "aws_amplify_branch" "production" {
+  app_id      = aws_amplify_app.web.id
+  branch_name = "main"
 
-#   framework = "React"
-#   stage     = "PRODUCTION"
-# }
+  framework = "React"
+  stage     = "PRODUCTION"
+}
