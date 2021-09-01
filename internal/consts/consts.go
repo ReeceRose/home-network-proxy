0d57bfac4ede94242f349aab45e0417a8e6456e2
Reece Rose <reecerose18@gmail.com>
2021-08-31 20:46:39 -0400

    Code cleanup

--- internal/consts/consts.go
+++ internal/consts/consts.go
@@ -3,6 +3,6 @@ package consts
 const (
 	// API_URL is a key used to lookup the api url (Used by: reporting-tool)
 	API_URL = "API_URL"
-	// REPORTING_TOOL_FILENAME is a key used to lookup the filename which contains a UUID (Used by: reporting-tool)
-	REPORTING_TOOL_FILENAME = "REPORTING_TOOL_FILENAME"
+	// REPORTING_TOOL_FILENAME is the filename of a file which contains a UUID (Used by: reporting-tool)
+	REPORTING_TOOL_FILENAME = "reporting-tool.json"
 )
