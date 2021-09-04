65cc70e8c075f7c8e9020fb30ece0335480225c4
Reece Rose <reecerose18@gmail.com>
2021-09-03 22:23:09 -0400

    Fixed aws-exports (#9)

--- web/aws-exports.js
+++ web/aws-exports.js
@@ -1,9 +1,10 @@
 const awsmobile = {
-  aws_project_region: process.env.project_region,
-  aws_cognito_identity_pool_id: process.env.cognito_identity_pool_id,
-  aws_cognito_region: process.env.cognito_region,
-  aws_user_pools_id: process.env._user_pools_id,
-  aws_user_pools_web_client_id: process.env.user_pools_web_client_id,
+  aws_project_region: 'us-east-1',
+  aws_cognito_identity_pool_id:
+    'us-east-1:94fc077c-1014-4af3-8755-a1f1b5ec9de7',
+  aws_cognito_region: 'us-east-1',
+  aws_user_pools_id: 'us-east-1_UqSbtPH1K ',
+  aws_user_pools_web_client_id: '5q42kpv9mh7spcjg63l88sqpah',
   oauth: {},
 };
 
