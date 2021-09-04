57fff276350cdfd7c098fd79ce95c93b1a2a3eff
Reece Rose <reecerose18@gmail.com>
2021-09-03 22:45:43 -0400

    Added new id for production auth (#9)

--- web/aws-exports.js
+++ web/aws-exports.js
@@ -4,7 +4,10 @@ const awsmobile = {
     'us-east-1:94fc077c-1014-4af3-8755-a1f1b5ec9de7',
   aws_cognito_region: 'us-east-1',
   aws_user_pools_id: 'us-east-1_UqSbtPH1K ',
-  aws_user_pools_web_client_id: '5q42kpv9mh7spcjg63l88sqpah',
+  aws_user_pools_web_client_id:
+    process.env.ENV == 'production'
+      ? '62tinnkha2neaa86d3o2j33m9g'
+      : '5q42kpv9mh7spcjg63l88sqpah',
   oauth: {},
 };
 
